ffc1fd2ac24f1fca71ada04fbb8cce1fd2cab573
andremichelle <andre.michelle@gmail.com>
2020-06-20 19:36:16 +0200

    clears VideoCreatorProgram

---
+++ src/main/kotlin/VideoCreatorProgram.kt
@@ -0,0 +1,14 @@
+import org.openrndr.application
+
+fun main() {
+    application {
+        configure {
+            width = 1280
+            height = 720
+        }
+        program {
+            extend {
+            }
+        }
+    }
+}
